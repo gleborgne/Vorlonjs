4c8f9cc61be34dab6c229479f5a7c2d7ebc09c12
RaphaelLichan <raphael.lichan@outlook.com>
2015-05-20 15:32:40 +0200

    Connection with the tab client chrome.

--- Plugins/Vorlon/plugins/sourceDebugger/vorlon.sourceDebugger.js
+++ Plugins/Vorlon/plugins/sourceDebugger/vorlon.sourceDebugger.js
@@ -1,55 +1,51 @@
-﻿module VORLON {
-    declare var $: any;
-    declare var io;
-
-    export class SRCDebugger extends Plugin {
-        // Client
-        private _timeoutId;
-        private item;
-
-        constructor() {
-            super("SrcDebugger", "control.html", "control.css");
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var VORLON;
+(function (VORLON) {
+    var SRCDebugger = (function (_super) {
+        __extends(SRCDebugger, _super);
+        function SRCDebugger() {
+            _super.call(this, "SrcDebugger", "control.html", "control.css");
+            this._protocolUrl = "/vorlon/plugins/sourceDebugger/protocol.json";
             this._ready = false;
             console.log("SrcDebugger loaded!");
         }
-
-        public getID(): string {
+        SRCDebugger.prototype.getID = function () {
             return "SrcDebugger";
-        }
-
-        private _markForRefresh() {
+        };
+        SRCDebugger.prototype._markForRefresh = function () {
+            var _this = this;
             if (this._timeoutId) {
                 clearTimeout(this._timeoutId);
             }
-
-            this._timeoutId = setTimeout(() => {
-                this.refresh();
+            this._timeoutId = setTimeout(function () {
+                _this.refresh();
             }, 10000);
-        }
-
-        public refresh(): void {
-        }
-
-        private _getTab(json: any): any {
+        };
+        SRCDebugger.prototype.refresh = function () {
+        };
+        SRCDebugger.prototype._getTab = function (json) {
             var find = null;
-            json.forEach((item) => {
+            json.forEach(function (item) {
                 if (item.type == "other" && item.title.toLowerCase().indexOf("demo") > 0) {
                     find = item;
                 }
             });
             return find;
-        }
-
-        private _packageJson(): any {
+        };
+        SRCDebugger.prototype._packageJson = function () {
             var packageJson = {
                 type: 'json',
                 json: JSON.stringify(this.item)
-            }
-
+            };
             return packageJson;
-        }
-
-        public onRealtimeMessageReceivedFromDashboardSide(receivedObject: any): void {
+        };
+        SRCDebugger.prototype.onRealtimeMessageReceivedFromDashboardSide = function (receivedObject) {
+            var _this = this;
             var json;
             var xhr = new XMLHttpRequest();
             var xd;
@@ -58,45 +54,39 @@
                     case "getJSON":
                         xhr.open("GET", "http://localhost:9222/json", true);
                         xhr.withCredentials = true;
-                        xhr.onreadystatechange = () => {
+                        xhr.onreadystatechange = function () {
                             if (xhr.readyState == 4 && xhr.status == 200) {
                                 json = JSON.parse(xhr.responseText);
-                                this.item = this._getTab(json);
-
-                                Core.Messenger.sendRealtimeMessage(this.getID(), this._packageJson(), RuntimeSide.Client);
+                                _this.item = _this._getTab(json);
+                                VORLON.Core.Messenger.sendRealtimeMessage(_this.getID(), _this._packageJson(), VORLON.RuntimeSide.Client);
                             }
-                        }
+                        };
                         xhr.send();
                         break;
                 }
                 return;
             }
-        }
-
-        // DASHBOARD
-        private _socket: any;
-        private _index: number;
-        private _protocolUrl: string = "/vorlon/plugins/sourceDebugger/protocol.json";
-        private _protocol;
-        public startDashboardSide(div: HTMLDivElement = null): void {
-            Core.Messenger.sendRealtimeMessage(this.getID(), {
+        };
+        SRCDebugger.prototype.startDashboardSide = function (div) {
+            var _this = this;
+            if (div === void 0) { div = null; }
+            VORLON.Core.Messenger.sendRealtimeMessage(this.getID(), {
                 type: "getJSON",
                 order: null
-            }, RuntimeSide.Dashboard);
+            }, VORLON.RuntimeSide.Dashboard);
             var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = () => {
+            xhr.onreadystatechange = function () {
                 if (xhr.readyState === 4) {
                     if (xhr.status === 200) {
-                        this._protocol = JSON.parse(xhr.responseText);
+                        _this._protocol = JSON.parse(xhr.responseText);
                     }
                 }
-            }
+            };
             xhr.open("GET", this._protocolUrl);
             xhr.send();
             this._index = 0;
             this._ready = true;
-        }
-
+        };
         //private addProtocol() {
         //    var domains = this._protocol.domains;
         //    for (var i = 0; i < domains.length; i++) {
@@ -107,7 +97,6 @@
         //        //        var events = {};
         //        //        var eventName = `${domain.domain}.${arguments[0]}`;
         //        //        console.log(eventName);
-
         //        //        //this.on.call(this, `${domain.domain}.${arguments[0]}`, arguments[1]);
         //        //    };
         //        //})(domain);
@@ -119,39 +108,37 @@
         //        }
         //    }
         //}
-
         //private _createCommand(domain: any, command: any) {
         //    console.log(domain.domain + "." + command.name);
         //}
-
-        private _connectWithClient(receivedObject: any): void {
+        SRCDebugger.prototype._connectWithClient = function (receivedObject) {
+            var _this = this;
             var json = JSON.parse(receivedObject.json);
             var url = json.webSocketDebuggerUrl;
             this._socket = new WebSocket(url);
             //this.addProtocol();
-            this._socket.onopen = () => {
-                if (this._socket.readyState === WebSocket.OPEN) {
-
+            this._socket.onopen = function () {
+                if (_this._socket.readyState === WebSocket.OPEN) {
                     var json = {
                         "method": "Debugger.enable",
-                        "id": this._index++
+                        "id": _this._index++
                     };
-                    this._socket.send(JSON.stringify(json));
+                    _this._socket.send(JSON.stringify(json));
                 }
             };
-            this._socket.onerror = () => {
+            this._socket.onerror = function () {
             };
-            this._socket.onmessage = (message) => {
+            this._socket.onmessage = function (message) {
                 var command = {};
                 if (message && message.data) {
                     var data = JSON.parse(message.data);
                     if (data.method && data.method === "Debugger.scriptParsed") {
                         command = {
-                            "id": this._index++,
+                            "id": _this._index++,
                             "method": "Debugger.getScriptSource",
                             "params": { "scriptId": data.params.scriptId.toString() }
                         };
-                        this._socket.send(JSON.stringify(command));
+                        _this._socket.send(JSON.stringify(command));
                     }
                     if (data && data.result) {
                         var result = JSON.parse(data.result);
@@ -161,14 +148,15 @@
                     }
                 }
             };
-        }
-
-        public onRealtimeMessageReceivedFromClientSide(receivedObject: any): void {
+        };
+        SRCDebugger.prototype.onRealtimeMessageReceivedFromClientSide = function (receivedObject) {
             if (receivedObject && receivedObject.type === "json") {
                 this._connectWithClient(receivedObject);
             }
-        }
-    }
-
-    Core.RegisterPlugin(new SRCDebugger());
-}
+        };
+        return SRCDebugger;
+    })(VORLON.Plugin);
+    VORLON.SRCDebugger = SRCDebugger;
+    VORLON.Core.RegisterPlugin(new SRCDebugger());
+})(VORLON || (VORLON = {}));
+//# sourceMappingURL=vorlon.sourceDebugger.js.map
